19981bda31a749804e3d2a5f973441352bef2546
Drew Cook <drewcook29@gmail.com>
2021-07-30 16:39:06 -0600

    UI updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'archives/index'
+  resources :entries
+  # Make root go directly to entries index
+  root to:"entries#index"
 end
