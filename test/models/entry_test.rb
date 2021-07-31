b8892d0f04e3c7ce35a63fdf6ff6346be39f6d18
Drew Cook <drewcook29@gmail.com>
2021-07-30 21:20:34 -0600

    tests

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with valid attributes" do
+    entry = Entry.new(meal_type: "Breakfast", calories: 300, proteins: 20, carbohydrates: 40, fats: 10)
+    assert entry.save
+  end
 end
